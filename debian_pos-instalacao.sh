19d9b5302f4428781ea0ea2228ebf07cae9a0065
Bruno Calado Ribeiro <plus.ultra.physis@gmail.com>
2012-01-28 22:26:07 -0200

    Atualiza??es de vers?es Aliases alterados

--- debian_pos-instalacao.sh
+++ debian_pos-instalacao.sh
@@ -1,32 +1,38 @@
-#!/bin/bash
+#!/bin/sh
 
 nomedousuario="bcr"
+distribuicao="squeeze"
 
 # --------------------------------------------------------------------------------
 # Debian Multimedia
 # --------------------------------------------------------------------------------
 echo >> /etc/apt/sources.list
-echo "deb http://www.debian-multimedia.org squeeze main" >> /etc/apt/sources.list
+echo "deb http://ftp.br.debian.org/debian-multimedia stable main" >> /etc/apt/sources.list
+echo "deb-src http://ftp.br.debian.org/debian-multimedia stable main" >> /etc/apt/sources.list
 
-wget --quiet "http://debian-multimedia.org/pool/main/d/debian-multimedia-keyring/debian-multimedia-keyring_2010.12.26_all.deb"
+# Faz o download da keyring
+wget --quiet "http://www.debian-multimedia.org/pool/main/d/debian-multimedia-keyring/debian-multimedia-keyring_2010.12.26_all.deb"
 dpkg --install "debian-multimedia-keyring_2010.12.26_all.deb"
 rm "debian-multimedia-keyring_2010.12.26_all.deb"
+aptitude update
 
 
 # --------------------------------------------------------------------------------
 # Debian Backports
 # --------------------------------------------------------------------------------
 echo >> /etc/apt/sources.list
-echo "deb http://backports.debian.org/debian-backports squeeze-backports main" >> /etc/apt/sources.list
+echo "deb http://backports.debian.org/debian-backports $distribuicao-backports main" >> /etc/apt/sources.list
+aptitude update
 
 
 # --------------------------------------------------------------------------------
 # Wine HQ
 # --------------------------------------------------------------------------------
 echo >> /etc/apt/sources.list
-echo "deb http://www.lamaresh.net/apt squeeze main" >> /etc/apt/sources.list
+echo "deb http://www.lamaresh.net/apt $distribuicao main" >> /etc/apt/sources.list
 
 wget -O - http://www.lamaresh.net/apt/key.gpg | apt-key add -
+aptitude update
 
 
 # --------------------------------------------------------------------------------
@@ -44,7 +50,7 @@ echo 'JAVA_HOME="/usr/lib/jvm/java-6-sun"' | tee -a /etc/environment
 # --------------------------------------------------------------------------------
 # Funcionalidades
 # --------------------------------------------------------------------------------
-# Comandos de root apenas com sudo sem senha
+# Comandos de root - sudo sem senha
 adduser $nomedousuario sudo
 echo >> /etc/sudoers
 echo "%sudo ALL=NOPASSWD: ALL" >> /etc/sudoers
@@ -59,10 +65,9 @@ echo "if [ -f ~/.bash_aliases ]; then"  >> /root/.bashrc
 echo "    . ~/.bash_aliases"            >> /root/.bashrc
 echo "fi"                               >> /root/.bashrc
 
-# Copia alias do bash
+# Copia aliases do bash
 cp -v systemFiles/.bash_aliases /root
 cp -v systemFiles/.bash_aliases /home/$nomedousuario/
-
 
 # --------------------------------------------------------------------------------
 # Vi Improved
@@ -94,153 +99,155 @@ cp -v bin/* /bin
 # Atualização Inicial
 # --------------------------------------------------------------------------------
 aptitude update
-aptitude -y full-upgrade
+aptitude --assume-yes upgrade
 
 
 # --------------------------------------------------------------------------------
 #  Multimedia
 # --------------------------------------------------------------------------------
-aptitude -y install vlc
-aptitude -y install mplayer
-aptitude -y install nrg2iso
-aptitude -y install bchunk          #bin2iso
-aptitude -y install audacity
-aptitude -y install avidemux 
-aptitude -y install recordmydesktop
+aptitude --assume-yes install vlc
+aptitude --assume-yes install nrg2iso
+aptitude --assume-yes install bchunk          #bin2iso
+aptitude --assume-yes install audacity
+aptitude --assume-yes install avidemux 
+# Record My Desktop
+aptitude --assume-yes install recordmydesktop
+aptitude --assume-yes install gtk-recordmydesktop
 
 
 # --------------------------------------------------------------------------------
 # Wine HQ
 # --------------------------------------------------------------------------------
-aptitude -y install wine
+aptitude --assume-yes install wine
 
 
 # --------------------------------------------------------------------------------
 # Virtual Box
 # --------------------------------------------------------------------------------
-aptitude -y install virtualbox-ose
+aptitude --assume-yes install virtualbox-ose
 
 
 # --------------------------------------------------------------------------------
 # Navegadores
 # --------------------------------------------------------------------------------
 # Google Chrome
-aptitude -y install libcurl3
-wget --quiet "https://dl-ssl.google.com/linux/direct/google-chrome-stable_current_i386.deb"
+aptitude --assume-yes install libcurl3
+wget "https://dl-ssl.google.com/linux/direct/google-chrome-stable_current_i386.deb"
 dpkg --install "google-chrome-stable_current_i386.deb"
 rm "google-chrome-stable_current_i386.deb"
 
-# Firefox 4
-wget --quiet "http://mozilla.c3sl.ufpr.br/releases//firefox/releases/4.0.1/linux-i686/en-US/firefox-4.0.1.tar.bz2"
-tar xjvf "firefox-4.0.1.tar.bz2"
-rm "firefox-4.0.1.tar.bz2"
-mv firefox /home/bcr
+# Firefox
+wget --quiet "http://mozilla.c3sl.ufpr.br/releases//firefox/releases/9.0.1/linux-i686/pt-BR/firefox-9.0.1.tar.bz2"
+tar xjvf "firefox-9.0.1.tar.bz2"
+rm "firefox-9.0.1.tar.bz2"
+mv firefox /home/$nomedousuario
 
 
 # --------------------------------------------------------------------------------
 # Adobe Flash
 # --------------------------------------------------------------------------------
-aptitude -y install flashplugin-nonfree
+aptitude --assume-yes install flashplugin-nonfree
 /usr/sbin/update-alternatives --config flash-mozilla.so
 
 
 # --------------------------------------------------------------------------------
 # Java SUN
 # --------------------------------------------------------------------------------
-aptitude -y install sun-java6-jdk
-aptitude -y install sun-java6-jre
-aptitude -y install sun-java6-plugin
+aptitude --assume-yes install sun-java6-jdk
+aptitude --assume-yes install sun-java6-jre
+aptitude --assume-yes install sun-java6-plugin
 update-java-alternatives -s java-6-sun
 
 
 # --------------------------------------------------------------------------------
 # Compactadores e Descompactadores
 # --------------------------------------------------------------------------------
-aptitude -y install bzip2
-aptitude -y install zip 
-aptitude -y install rar
-aptitude -y install p7zip-full
+aptitude --assume-yes install bzip2
+aptitude --assume-yes install zip 
+aptitude --assume-yes install rar
+aptitude --assume-yes install p7zip-full
 
 
 # --------------------------------------------------------------------------------
 # SSH
 # --------------------------------------------------------------------------------
-aptitude -y install openssh-server
+aptitude --assume-yes install openssh-server
 
 
 # --------------------------------------------------------------------------------
 # Aparência
 # --------------------------------------------------------------------------------
-aptitude -y install colorgcc
-aptitude -y install colordiff
-aptitude -y install colormake
-aptitude -y install colortail
+aptitude --assume-yes install colorgcc
+aptitude --assume-yes install colordiff
+aptitude --assume-yes install colormake
+aptitude --assume-yes install colortail
 # Fontes
-aptitude -y install xfonts-terminus
-aptitude -y install xfonts-terminus-oblique
-aptitude -y install xfonts-mona
-aptitude -y install ttf-georgewilliams
-aptitude -y install ttf-nafees
-aptitude -y install ttf-freefont
-aptitude -y install ttf-bitstream-vera
+aptitude --assume-yes install xfonts-terminus
+aptitude --assume-yes install xfonts-terminus-oblique
+aptitude --assume-yes install xfonts-mona
+aptitude --assume-yes install ttf-georgewilliams
+aptitude --assume-yes install ttf-nafees
+aptitude --assume-yes install ttf-freefont
+aptitude --assume-yes install ttf-bitstream-vera
 
 
 # --------------------------------------------------------------------------------
 # Funcionalidades
 # --------------------------------------------------------------------------------
-aptitude -y install bash-completion
+aptitude --assume-yes install bash-completion
 
 
 # --------------------------------------------------------------------------------
 # System Tools
 # --------------------------------------------------------------------------------
-aptitude -y install htop
-aptitude -y install iftop
-aptitude -y install galternatives
+aptitude --assume-yes install htop
+aptitude --assume-yes install iftop
+aptitude --assume-yes install galternatives
+aptitude --assume-yes install macchanger
 
 
 # --------------------------------------------------------------------------------
 # Atualização de Horário e Data 
 # --------------------------------------------------------------------------------
-aptitude -y install ntpdate
-ntpdate br.pool.ntp.org br.pool.ntp.org br.pool.ntp.org #Atualização da hora
+#aptitude --assume-yes install ntpdate
+#ntpdate br.pool.ntp.org br.pool.ntp.org br.pool.ntp.org #Atualização da hora
 
 
 # --------------------------------------------------------------------------------
 # Development
 # --------------------------------------------------------------------------------
-aptitude -y install gcc
-aptitude -y install g++
-aptitude -y install make
-aptitude -y install python3
-aptitude -y install binutils
-aptitude -y install libstdc++5
-aptitude -y install mercurial
+aptitude --assume-yes install gcc
+aptitude --assume-yes install g++
+aptitude --assume-yes install make
+aptitude --assume-yes install python3
+aptitude --assume-yes install binutils
+aptitude --assume-yes install libstdc++5
+aptitude --assume-yes install mercurial
 
 
 # --------------------------------------------------------------------------------
 # Linux Development
 # --------------------------------------------------------------------------------
-aptitude -y install build-essential # Package Development
+aptitude --assume-yes install build-essential # Package Development
 
 
 # --------------------------------------------------------------------------------
 # Linux Kernel
 # --------------------------------------------------------------------------------
-aptitude -y install linux-headers-$(uname -r)
+aptitude --assume-yes install linux-headers-$(uname -r)
 
 
 # --------------------------------------------------------------------------------
 # Editores
 # --------------------------------------------------------------------------------
-aptitude -y install vim 
+aptitude --assume-yes install vim 
 
 
 # --------------------------------------------------------------------------------
 # Diversos
 # --------------------------------------------------------------------------------
-aptitude -y install webhttrack
-aptitude -y install hdparm 
+aptitude --assume-yes install webhttrack
+aptitude --assume-yes install hdparm 
 
 
 # --------------------------------------------------------------------------------
@@ -250,14 +257,14 @@ wget --quiet "http://ufpr.dl.sourceforge.net/project/file-folder-ren/Metamorphos
 dpkg --install "metamorphose_1.1.2-2_all.deb"
 rm "metamorphose_1.1.2-2_all.deb"
 
-aptitude -y install gprename
+aptitude --assume-yes install gprename
 
 
 # --------------------------------------------------------------------------------
 # Dropbox
 # --------------------------------------------------------------------------------
-aptitude -y install libnautilus-extension-dev
-aptitude -y install python-docutils
+aptitude --assume-yes install libnautilus-extension-dev
+aptitude --assume-yes install python-docutils
 wget --quiet "http://www.dropbox.com/download?dl=packages/nautilus-dropbox-0.6.7.tar.bz2"
 tar xjvf "nautilus-dropbox-0.6.7.tar.bz2"
 cd nautilus-dropbox-0.6.7
@@ -266,6 +273,12 @@ make
 make install
 dropbox start -i
 rm -r "nautilus-dropbox-0.6.7.tar.bz2" nautilus-dropbox-0.6.7
+
+
+
+# --------------------------------------------------------------------------------
+# 
+# --------------------------------------------------------------------------------
 
 
 
